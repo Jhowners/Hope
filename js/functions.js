382106fd573d3355b535663ba23fd14f957f919d
Jhowners <jhonpeterson575@hotmail.com>
2023-02-08 19:23:45 -0300

    adding js to files and organizating everything

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+// const button = document.querySelector('idLogin');
+
+// let person = {
+//     username: document.getElementById('idUsername').value,
+//     password: document.getElementById('idPassword').value
+// }
+
+
+
+// function validaCampos(){
+//     // debugger
+    
+//     console.log(this.person.password, this.person.username)
+// }
+
+// document.getElementById('idLogin').onclick = function(){
+//     validaCampos()
+// }
+
+// button.addEventListener("click", function(event){
+//     validaCampos();
+// })
