edebc4bc2f773523bc5dfed5a8ef7770c31f196f
Jhowners <jhonpeterson575@hotmail.com>
2023-02-10 11:12:07 -0300

    CreateAccPage and Functions

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,26 @@
-// const button = document.querySelector('idLogin');
+function validaCampos(person){
+    // debugger
+    console.log(person.username, person.password);
+    if(!person.username) {
+        return document.getElementById('idError').innerHTML = "Invalid Username"
+    }
 
-// let person = {
-//     username: document.getElementById('idUsername').value,
-//     password: document.getElementById('idPassword').value
-// }
+    if(!person.password) {
+        return document.getElementById('idError').innerHTML = "Invalid Password"
+    }
+}
 
-
-
-// function validaCampos(){
-//     // debugger
+function login(){
+    debugger
+    let person = {
+        username: document.getElementById('idUsername').value,
+        password: document.getElementById('idPassword').value
+    };
     
-//     console.log(this.person.password, this.person.username)
-// }
-
-// document.getElementById('idLogin').onclick = function(){
-//     validaCampos()
-// }
-
+    if (!validaCampos(person)) {
+    document.getElementById('form').submit();
+    }
+}
 // button.addEventListener("click", function(event){
 //     validaCampos();
 // })
