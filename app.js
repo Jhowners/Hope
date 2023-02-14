8f05b9e5caad9b3cc254e59750737e532b980695
Jhowners <jhonpeterson575@hotmail.com>
2023-02-14 16:03:56 -0300

    Creating a connection between api and mysql

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const mysql = require('mysql')
+
+
+
+app.use(morgan('combined'))
+
+app.get('/users/:id', (req, res) => {
+    console.log("Fethcing user with id: " + req.params.id)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'Hehe'
+    })
+    connection.query("SELECT * FROM users", (err, rows, fields) =>{
+        console.log("i think we fetched users successfully")
+        res.json(rows)
+    })
+    //res.end()
+})
 
 app.get("/", (req, res) => {
     console.log("Responding to root route")
@@ -7,9 +28,9 @@ app.get("/", (req, res) => {
 })
 
 app.get("/users",(req, res) => {
-    var user1 = {firstName: "Anthony", lastName: "Don"}
-    var user1 = {firstName: "Mario", lastName: "Kim"}
-    res.json(user1)
+    var user1 = {firstName: "Anthony", lastName: "Don"};
+    var user2 = {firstName: "Mario", lastName: "Kim"}
+    res.json([user1, user2])
 })
 
 //localhost:3003
